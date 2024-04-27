282310698f4c8a966362552501adbbbe06220261
Lavanya2252 <lavanyaaa2252@gmail.com>
2024-04-27 18:42:51 +0530

    Learning Forms and CRUD operations

--- FoodApp/FoodApp/settings.py
+++ FoodApp/FoodApp/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'food/static')]
 # VENV_PATH = os.path.dirname(BASE_DIR)
 # STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 
